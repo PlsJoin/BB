11320434194de7ee9a45b2d9ee92c4e4a992ea0d
Multi Trillion <synk8824@gmail.com>
2026-09-22 21:54:46 +0900

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,19 +1,7 @@
 const questions = [
     {
-        id: "fuji",
-        question: "日本で一番高い山の標高を調べろ",
-        answer: "3776"
-    },
-
-    {
-        id: "question2",
-        question: "日本の首都はどこか調べろ",
-        answer: "東京"
-    },
-
-    {
-        id: "question3",
-        question: "水の沸点は何℃か調べろ",
-        answer: "100"
+        id: "railway",
+        question: "普通鉄道で、駅間距離が最も短いとされる区間の距離を調べろ",
+        answer: "0.2km"
     }
 ];
