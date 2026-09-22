869658dad7aa54eb405bcdc41c79d4aab53fa00a
Multi Trillion <synk8824@gmail.com>
2026-09-22 21:49:30 +0900

    Create questions.js

---
+++ js/questions.js
@@ -0,0 +1,19 @@
+const questions = [
+    {
+        id: "fuji",
+        question: "日本で一番高い山の標高を調べろ",
+        answer: "3776"
+    },
+
+    {
+        id: "question2",
+        question: "日本の首都はどこか調べろ",
+        answer: "東京"
+    },
+
+    {
+        id: "question3",
+        question: "水の沸点は何℃か調べろ",
+        answer: "100"
+    }
+];
